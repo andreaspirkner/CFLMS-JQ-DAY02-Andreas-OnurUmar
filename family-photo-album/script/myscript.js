217e2315cc9a4167c27373bc8822787e9166dff2
onurumar <65164611+onurumar@users.noreply.github.com>
2020-06-10 14:24:43 +0200

    myversionofphotoalbum

---
+++ family-photo-album/script/myscript.js
@@ -0,0 +1,51 @@
+
+
+var familyAndFriends = JSON.parse(ffdata);
+console.log(familyAndFriends);
+
+
+$(document).ready(function() {
+    for (var i = 0; i < familyAndFriends.length; i++) {
+        
+        var person = $(`
+		<div class="person">
+            <img src="${familyAndFriends[i].image}" alt="Team Member">
+            <p>${familyAndFriends[i].name}</p>
+            <div class="overlay">
+                <div id="info" class="overlay_text">
+                    <p>${familyAndFriends[i].relation}</p>
+                    <p>${familyAndFriends[i].email}</p>
+                    <p class="small">tel: ${familyAndFriends[i].tel}</p>
+                </div>
+            </div>
+        </div>
+	`);
+   
+    $("div#friends-family").append(person);
+    
+        var borderStyle = "";
+        if (familyAndFriends[i].relation == "sister") {
+            //borderStyle = "1px solid blue";
+            $(`div.person:eq(${i})`).addClass("sister")
+        } else if (familyAndFriends[i].relation == "friend") {
+            //borderStyle = "1px solid red";
+            $(`div.person:eq(${i})`).addClass("friend")
+        } else if (familyAndFriends[i].relation == "cousine") {
+            //borderStyle = "1px solid yellow"
+            $(`div.person:eq(${i})`).addClass("cousine")
+        }
+
+    }
+
+    var everyperson = $(this).find(".person");
+    console.table(everyperson)
+
+    for (let i = 0; i < everyperson.length; i++) {
+        $(everyperson[i]).on('click',function(){
+            $(this).remove();
+        })
+    }
+
+
+
+});
